f731a4db15706eddab724ccc9a027b67b34b8a63
Saktiranjan693 <saktiranjanbarik45@gmail.com>
2026-02-09 17:58:40 +0530

    jsc3

--- Javascript/class 3/script.js
+++ Javascript/class 3/script.js
@@ -10,6 +10,145 @@ console.log(a/b);
 console.log(a%b);
 console.log(a==b);
 
+// Assignment operator
+let x=10 
+    x+=6  // x= x+6
+    console.log(x);
+    x-=2
+    console.log(x);
+    x*=2
+    console.log(x);
+    x/=2
+    console.log(x);
+    x**=3
+    console.log(x);
+     x%=3
+    console.log(x);
+
+    // Logical Operator
+
+    let p=true
+    let q=true
+    let r=false
+
+    // AND 
+     console.log(p && q);
+     console.log(p && r);
+     console.log(r && r);
+    // OR
+    console.log(p || q);
+    console.log(p || r);
+    console.log(r || r);
+
+    //* NOT
+    console.log(!p);
+    console.log(!q);
+    console.log(!r);
+
+
+    //? COMPARISION OPERATOR
+
+    let c=10
+    let d=10
+    let e='10'
+    let f=20
+
+    console.log(c==d);
+    console.log(c==e);
+    console.log(c==f);
+    console.log(c!=d);
+    console.log(c!=e);
+    console.log(c!=f);
+    console.log(c===d);
+    console.log(c===e);
+    console.log(c===f);
+    console.log(c!==d);
+    console.log(c!==e);
+    console.log(c!==f);
+    console.log(c>d);
+    console.log(c>f);
+    console.log(c<d);
+    console.log(c<f);
+    console.log(c<=d);
+    console.log(c<=f);
+    console.log(c>=d);
+    console.log(c>=f);
+
+
+//? Unary Operator
+           let num=10
+    // INCREMENT 
+          // Pre-increment
+            console.log(++num);
+
+            // Post-increment
+             console.log(num--);
+     
+            // eg
+              console.log(++num);
+
+                 console.log(num++);
+    // DECREMENT 
+          //Pre-decrement
+          console.log(--num);
+
+          // post-decrement
+          console.log(num--);
+
+
+          // eg
+          console.log(--num);
+
+          console.log(num--);
+    //EXAMPLE
+          let number=290
+
+          console.log(number);
+          console.log(++number);
+          console.log(number++);
+          console.log(number--);
+          console.log(--number);
+          console.log(++number);
+          console.log(--number);
+          console.log(--number);
+          console.log(number);
+          console.log(number++);
+          console.log(number--);
+          console.log(number--);
+
+          
+
+
+          
+          
+    
+    
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+    
+
+
+     
+    
 
 
 
