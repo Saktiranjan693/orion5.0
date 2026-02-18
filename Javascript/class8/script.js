8aa05ab8946ee129fa24d7ea2c906b02b1d8dcd0
Saktiranjan693 <saktiranjanbarik45@gmail.com>
2026-02-18 17:14:11 +0530

    jsc8

--- Javascript/class8/script.js
+++ Javascript/class8/script.js
@@ -1,2 +1,55 @@
 // String and It's inbuilt Methods
 
+let str='lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.';
+
+console.log(str);
+console.log(str[2]);
+
+
+let name='sakti'
+let age=21
+let job='software deveplore'
+
+
+//? Template string
+
+let example ="hello iam sakti"
+
+//? Template Literal
+// ${example}
+
+//? Template Interpolation
+
+console.log(`my name id ${name}, age is ${age} and my job is${job}`);
+
+
+
+//?  Methods 
+
+       //? charAt()
+       console.log(str.charAt(2));
+
+
+       //? indexOf()
+
+       console.log(str.indexOf('ipsum'));
+
+       //? lastIndexOf()
+
+       console.log(str.lastIndexOf('ipsum'));
+
+       //? toUppercase
+       console.log(str.toUpperCase());
+
+       //? toLowercase()
+       console.log(str.toLowerCase());
+
+       //?slic()
+       
+       
+       
+       
+       
+
+ 
+
