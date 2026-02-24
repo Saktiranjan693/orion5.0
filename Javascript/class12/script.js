ce31ccd209b04e6b374c2d5f76a9cf6b8fa9c296
Saktiranjan693 <saktiranjanbarik45@gmail.com>
2026-02-24 17:57:35 +0530

    jsc12

--- Javascript/class12/script.js
+++ Javascript/class12/script.js
@@ -1,22 +1,71 @@
 const button=document.getElementById("btn");
 
-button.onclick=(e)=>{
-    console.log("button clicked");
-    button.style.backgroundColor="red";
-    console.log(e);
-    console.log(e.target);
-    console.log(e.type);
+// button.onclick=(e)=>{
+//     console.log("button clicked");
+//     button.style.backgroundColor="red";
+//     console.log(e);
+//     console.log(e.target);
+//     console.log(e.type);
    
-}
+// }
 
 
-const input=document.getElementById("input");
+// const input=document.getElementById("input");
 
-input.onchange=(e)=>{
-    console.log(e.target);
-    console.log(e.type);
+// input.onchange=(e)=>{
+//     console.log(e.target);
+//     console.log(e.type);
+//     console.log(e.target.value);
+    
+// }
+
+
+input.addEventListener("input",(e)=>{
     console.log(e.target.value);
-    
-    
+})
+
+
+function handleClick(){
+    console.log("button clicked");
     
 }
+button.addEventListener("click",handleClick);
+
+setTimeout(()=>{
+    button.removeEventListener("click",handleClick);
+    console.log("button removed");
+    
+},5000)
+
+
+
+//! prototype
+
+const person1={
+    name:"john",
+    age:30,
+    city:"new york"
+
+}
+ const person2={
+
+    cours:"MERN" , skils:["Html","Css","JavaScript"], __proto__:person1
+}
+
+person1.newkey="new value";
+console.log(person1.newkey);
+
+console.log(person1.name);
+console.log(person2.name);
+
+
+const person3=Object.create(person1);
+console.log(person3.name);
+person3.isworking=true;
+console.log(person3.isworking);
+console.log(person2);
+console.log(person3);
+
+
+
+
