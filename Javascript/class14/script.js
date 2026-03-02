27e54c3f39216a5e435258d0a6e2259b1c62c8b6
Saktiranjan693 <saktiranjanbarik45@gmail.com>
2026-03-02 18:01:55 +0530

    JsC14

--- Javascript/class14/script.js
+++ Javascript/class14/script.js
@@ -1,23 +1,23 @@
 // Sychronous and Asynchronous
 
-// Synchronous
-console.log("Hello");
-console.log("World");
+// // Synchronous
+// console.log("Hello");
+// console.log("World");
 
-// Asynchronous
+// // Asynchronous
 
-console.log("one");
-console.log("two");
-console.log("three");
-console.log("four");
-console.log("five");
+// console.log("one");
+// console.log("two");
+// console.log("three");
+// console.log("four");
+// console.log("five");
 
 
-setTimeout(() => {
-    console.log("six");
-}, 1000); 
+// setTimeout(() => {
+//     console.log("six");
+// }, 1000); 
 
-console.log("seven");
+// console.log("seven");
 
 //! clallback hell
 
@@ -37,35 +37,77 @@ console.log("seven");
 // }
 
 
-function geruser(userId,callback){
-    setTimeout(() => {
-        console.log("getting user from database...");
-        callback({userId: userId, username: "john"});
+// function geruser(userId,callback){
+//     setTimeout(() => {
+//         console.log("getting user from database...");
+//         callback({userId: userId, username: "john"});
         
-    }, 1000);
-}
+//     }, 1000);
+// }
 
-function getuserPosts(username,callback){
-    setTimeout(()=>{
-        console.log("getting posts for user...");
-        callback([{postId: 1, title: "post 1"}, {postId: 2, title: "post 2"}]);
+// function getuserPosts(username,callback){
+//     setTimeout(()=>{
+//         console.log("getting posts for user...");
+//         callback([{postId: 1, title: "post 1"}, {postId: 2, title: "post 2"}]);
 
-    },1000);
-}
+//     },1000);
+// }
 
-function getPostComments(postId,callback){
-    setTimeout(()=>{
-        console.log("getting comments for post...");
-        callback([{commentID:101,text:"nice post!"}, {commentID:102,text:"thanks for sharing!"}]);
+// function getPostComments(postId,callback){
+//     setTimeout(()=>{
+//         console.log("getting comments for post...");
+//         callback([{commentID:101,text:"nice post!"}, {commentID:102,text:"thanks for sharing!"}]);
+//     })
+// }
+
+// //nested callbacks (callback hell)
+
+// getuserPosts(1,(user)=>{
+//     getuserPosts(user.username,(posts)=>{
+//         getPostComments(posts[0].postId,(comments)=>{
+//             console.log(comments);
+//         });
+// });     
+// });
+
+
+//! Promises
+// pending
+// resolved
+// rejected
+
+
+//? .then()
+//? .catch()
+
+// Promise  
+//    .then(()=>{
+
+//    })
+//    .catch(()=>{
+
+//    })
+
+
+//? fetch()
+
+// fetch(api)
+//     .then((res)=>{
+
+//     })
+//     .catch((err)=>{
+//         console.log("Error fetching data:", err);
+//     })
+
+
+fetch("  https://jsonplaceholder.typicode.com/posts")
+   .then((res)=>{
+    console.log("Response received",res);
+    res.json().then((data)=>{
+        console.log("Data received:", data);
+    });
+   })
+    .catch((err)=>{
+        console.log("Error fetching data:", err);
     })
-}
 
-//nested callbacks (callback hell)
-
-getuserPosts(1,(user)=>{
-    getuserPosts(user.username,(posts)=>{
-        getPostComments(posts[0].postId,(comments)=>{
-            console.log(comments);
-        });
-});     
-});
